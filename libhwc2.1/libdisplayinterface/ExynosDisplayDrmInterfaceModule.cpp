6700e3138864d7fcc5390b38de6ce8d997871bb0
Joe Shih <joeshih@google.com>
2023-10-18 07:39:22 +0000

    Revert "displaycolor: Add support for external displays"
    Revert submission 25035359-hwc-displaycolor-ext-display
    
    Reason for revert: DroidMonitor-triggered revert due to breakage b/306074705
    
    Reverted changes: /q/submissionid:25035359-hwc-displaycolor-ext-display
    
    Change-Id: I0ae3662932baf6b9c8b9865420bcad6ee23e39fd

--- libhwc2.1/libdisplayinterface/ExynosDisplayDrmInterfaceModule.cpp
+++ libhwc2.1/libdisplayinterface/ExynosDisplayDrmInterfaceModule.cpp
@@ -440,26 +440,14 @@ uint32_t ExynosDisplayDrmInterfaceModule::SaveBlob::getBlob(uint32_t type)
 
 void ExynosDisplayDrmInterfaceModule::getDisplayInfo(
         std::vector<displaycolor::DisplayInfo> &display_info) {
-    displaycolor::DisplayInfo disp_info;
+    displaycolor::DisplayInfo primary_display;
 
-    disp_info.brightness_ranges = mExynosDisplay->mBrightnessController->getBrightnessRanges();
-    if (mExynosDisplay->mType == HWC_DISPLAY_PRIMARY) {
-        disp_info.panel_name = GetPanelName();
-        disp_info.panel_serial = GetPanelSerial();
-        if (mExynosDisplay->mIndex == 0)
-            disp_info.display_type = DisplayType::DISPLAY_PRIMARY;
-        else
-            disp_info.display_type = DisplayType::DISPLAY_SECONDARY;
-    } else if (mExynosDisplay->mType == HWC_DISPLAY_EXTERNAL) {
-        disp_info.display_type = DisplayType::DISPLAY_EXTERNAL;
-        disp_info.panel_name = "external_display";
-        disp_info.panel_serial = "0001";
-    } else {
-        ALOGE("Unsupported display type (%d) in getDisplayInfo!", mExynosDisplay->mType);
-        return;
-    }
+    primary_display.brightness_ranges =
+            mExynosDisplay->mBrightnessController->getBrightnessRanges();
+    primary_display.panel_name = GetPanelName();
+    primary_display.panel_serial = GetPanelSerial();
 
-    display_info.push_back(disp_info);
+    display_info.push_back(primary_display);
 }
 
 const std::string ExynosDisplayDrmInterfaceModule::GetPanelInfo(const std::string &sysfs_rel,
