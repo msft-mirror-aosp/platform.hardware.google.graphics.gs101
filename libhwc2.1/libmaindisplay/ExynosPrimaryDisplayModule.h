2aac9121660b223749a9543493956608f164d250
Sergey Volk <servolk@google.com>
2024-01-04 01:12:37 +0000

    libhwc2.1: Add getDisplayColorManager API
    There are a few code locations in HWC dealing with DPP/DQE handling, where
    we need to be able to obtain the ColorManager interface for an ExynosDisplay
    object for both primary and external displays. So adding this new API to
    ExynosDeviceModule for that.
    
    Bug: 299682821
    Test: manual & presubmit
    Change-Id: I1a2fbbf09ba9527b05eb6f209534307aa0467a9f

--- libhwc2.1/libmaindisplay/ExynosPrimaryDisplayModule.h
+++ libhwc2.1/libmaindisplay/ExynosPrimaryDisplayModule.h
@@ -162,6 +162,8 @@ class ExynosPrimaryDisplayModule : public ExynosPrimaryDisplay {
 
         int32_t updateBrightnessTable();
 
+        ColorManager* getColorManager() { return mColorManager.get(); }
+
     private:
         int32_t setLayersColorData();
         std::unique_ptr<ColorManager> mColorManager;
